e2eb2d19dd3360cf0978454ac02af4d5140324f8
thisiscpta <121990524+thisiscpta@users.noreply.github.com>
2023-03-03 10:04:55 +0800

    TP-20 print 2 lines
    done

--- src/main/java/DeepThought.java
+++ src/main/java/DeepThought.java
@@ -7,17 +7,17 @@ public class DeepThought {
 
     public int answer_to_the_ultimate_question_of_life_the_universe_and_everything(){
         System.out.println("You have asked the answer to the Ultimate Question of Life, the Universe, and Everything");
-        System.out.println("I need to think a while about that one, come back again in 10.0 million years...");
-        calculate(); // for 10.0 million years....
+        System.out.println("I need to think a while about that one, come back again in 2.0 million years...");
+        calculate(); // for 2.0 million years....
         return give_answer();
     }
 
     private void calculate(){
-        for(int million_years = 0; million_years < 11; million_years++){
+        for(int million_years = 0; million_years < 3; million_years++){
             System.out.println(million_years + " million years have passed...");
             think(1);
         }
-        System.out.println("10.0 million years have now passed and I have a answer");
+        System.out.println("2.0 million years have now passed and I have a answer");
     }
 
     private void think(int time){
