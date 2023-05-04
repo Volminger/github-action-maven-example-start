89c8f31df9c4707b8dfc263badf5941afd939f2e
rajubhovi1111111111 <79203757+rajubhovi1111111111@users.noreply.github.com>
2023-05-05 01:00:58 +0530

    Update TestDeepThought.java

--- src/test/java/TestDeepThought.java
+++ src/test/java/TestDeepThought.java
@@ -1,4 +1,4 @@
-/*import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
@@ -14,4 +14,4 @@ public class TestDeepThought {
             assertEquals( 42 , superComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything());
         }
 
-}*/
+}
