f7133a28f8b80b3631e97de36d2b878f5c112de9
Hamritha02 <106836018+Hamritha02@users.noreply.github.com>
2023-08-31 22:57:07 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,10 +2,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("I am your awesome Java application!");
-
         DeepThought ourSuperComputer = new DeepThought();
         int ans = ourSuperComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything();
-        System.out.println(ans);
+
+        RedisJava cache = new RedisJava();
+        cache.cache_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything",String.valueOf(ans));
+
+        System.out.println("Cached value: " + cache.get_cached_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything"));
     }
 }
