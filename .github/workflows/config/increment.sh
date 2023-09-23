c345cc84d20a034668307afdac09b15abc3a0195
Preethi Agnes <preethiagnest@gmail.com>
2023-09-23 23:32:25 +0530

    versioning added

--- .github/workflows/config/increment.sh
+++ .github/workflows/config/increment.sh
@@ -1,42 +1,7 @@
 #!/bin/bash
 
- 
+# Set the new version
+new_version=1.2.3  # Replace with your desired versioning logic
 
-# Input: Current version (e.g., "1.2.3")
-
-current_version="$1"
-
- 
-
-# Split the version into major, minor, and patch components
-
-IFS='.' read -ra version_parts <<< "$current_version"
-
-major="${version_parts[0]}"
-
-minor="${version_parts[1]}"
-
-patch="${version_parts[2]}"
-
- 
-
-# Increment the version based on SemVer rules
-
-# You can customize this logic to match your versioning strategy
-
-new_patch=$((patch + 1))
-
- 
-
-# Construct the new version string
-
-new_version="$major.$minor.$new_patch"
-
- 
-
-# Output the new version
-
-echo "$new_version"
-
-
-
+# Use the Maven Versions Plugin to update the version in the pom.xml
+mvn versions:set -DnewVersion=$new_version
