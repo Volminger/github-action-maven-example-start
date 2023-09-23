4e18420617b4edf37da8cba8d54cd7b085a8d772
Preethi Agnes <preethiagnest@gmail.com>
2023-09-24 00:33:35 +0530

    versioning added

--- .github/workflows/config/increment.sh
+++ .github/workflows/config/increment.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# Set the new version
-current_version="1.2.3"  # Replace with your desired versioning logic
+# Set the current version
+current_version="0.0.1"
 
-new_version=$((current_version + 1))
+# Split the version string into an array using '.' as the delimiter
+IFS='.' read -ra version_parts <<< "$current_version"
 
+# Extract major, minor, and patch components
+major="${version_parts[0]}"
+minor="${version_parts[1]}"
+patch="${version_parts[2]}"
+
+# Increment the desired component, for example, increment the patch version
+new_patch=$((patch + 1))
+
+# Construct the new version string
+new_version="$major.$minor.$new_patch"
 
 # Output the new version
 echo "$new_version"
