e21b0590291a85120906779652a4aff65cfefa60
Preethi Agnes <preethiagnest@gmail.com>
2023-09-24 00:31:35 +0530

    versioning added

--- .github/workflows/config/increment.sh
+++ .github/workflows/config/increment.sh
@@ -3,19 +3,8 @@
 # Set the new version
 current_version="1.2.3"  # Replace with your desired versioning logic
 
-# Split the version string into an array using IFS (Internal Field Separator)
-IFS='.' read -ra version_parts <<< "$current_version"
+new_version=$((current_version + 1))
 
-major="${version_parts[0]}"
-minor="${version_parts[1]}"
-patch="${version_parts[2]}"
-
-# Increment the version based on SemVer rules
-# You can customize this logic to match your versioning strategy
-new_patch=$((patch + 1))
-
-# Construct the new version string
-new_version="$major.$minor.$new_patch"
 
 # Output the new version
 echo "$new_version"
